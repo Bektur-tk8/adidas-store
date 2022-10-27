eef7e7a397d615a6a2933e1185a5773580fbd0a0
Bektur Tologonov <tbektur7@gmail.com>
2022-10-27 14:46:41 +0600

    productdetail add

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('latest-products/', views.LatestProductlist.as_view()),
-    
+    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view())
+      
 ]
--- product/views.py
+++ product/views.py
@@ -4,6 +4,7 @@ from .serializers import ProductSerializer
 from .models import Product
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 
 
 class LatestProductlist(APIView):
@@ -12,3 +13,15 @@ class LatestProductlist(APIView):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, category_slug, product_slug):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
